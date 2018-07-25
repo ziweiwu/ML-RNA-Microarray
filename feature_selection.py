8abe903b673c809bdbced3cded4b939def294b30
ziwei wu <ziweiwu@gmail.com>
2018-07-24 22:12:08 -0400

    updated python notebook

--- feature_selection.py
+++ feature_selection.py
@@ -2,9 +2,9 @@ import json
 import pandas as pd
 import numpy as np
 from sklearn import svm, ensemble, linear_model
-from sklearn.model_selection import KFold, cross_val_score, GridSearchCV
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.feature_selection import SelectFromModel
-from sklearn.metrics.classification import classification_report, jaccard_similarity_score, f1_score
+from sklearn.metrics.classification import jaccard_similarity_score
 
 # from model_evaluation import plot_confusion_matrix
 
@@ -40,63 +40,73 @@ def kfold_model_score(model, X_train, Y_train, numFolds=5):
     return np.mean(cross_val_score(model, X_train, Y_train, cv=k_fold_shuttle))
 
 
-# test alpha value in increasing order
-alpha_params = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 5, 50, 100, 200, 500, 1000]
-
-
-def tune_alpha(alpha_params, X_train, Y_train, X_test, Y_test):
-    for alpha in alpha_params:
-        model = linear_model.SGDClassifier(random_state=100, alpha=alpha, n_jobs=-1, penalty="l1", tol=1e-3)
-        model = model.fit(X_train, Y_train)
-        model_pred = model.predict(X_test)
-        model_score = jaccard_similarity_score(Y_test, model_pred)
-        print("Linear SVC score with alpha {:5f}: {:5f}".format(alpha, model_score.mean()))
-
-
-tune_alpha(alpha_params, X_train, Y_train, X_test, Y_test)
-
-# test the effect of number of tree on accuracy for random forest
-rf_clf = ensemble.RandomForestClassifier(random_state=100, n_jobs=-1)
-trees = [5, 10, 15, 20, 25, 50, 75, 100]
-
-
-def tune_trees(trees, X_train, Y_train, X_test, Y_test):
-    for tree in trees:
-        model = ensemble.RandomForestClassifier(random_state=100, n_jobs=-1, n_estimators=tree)
-        model = model.fit(X_train, Y_train)
-        model_pred = model.predict(X_test)
-        model_score = jaccard_similarity_score(Y_test, model_pred)
-        print("Random Forest score with {} trees: {:5f}".format(tree, model_score.mean()))
-
-
-tune_trees(trees, X_train, Y_train, X_test, Y_test)
+# # test alpha value in increasing order
+# alpha_params = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 5, 50, 100, 200, 500, 1000]
+#
+#
+# def tune_alpha(alpha_params, X_train, Y_train, X_test, Y_test):
+#     for alpha in alpha_params:
+#         model = linear_model.SGDClassifier(random_state=100, alpha=alpha, n_jobs=-1, penalty="l1", tol=1e-3)
+#         model = model.fit(X_train, Y_train)
+#         model_pred = model.predict(X_test)
+#         model_score = jaccard_similarity_score(Y_test, model_pred)
+#         print("Linear SVC score with alpha {:5f}: {:5f}".format(alpha, model_score.mean()))
+#
+#
+# tune_alpha(alpha_params, X_train, Y_train, X_test, Y_test)
+#
+# # test the effect of number of tree on accuracy for random forest
+# rf_clf = ensemble.RandomForestClassifier(random_state=100, n_jobs=-1)
+# trees = [5, 10, 15, 20, 25, 50, 75, 100]
+#
+#
+# def tune_trees(trees, X_train, Y_train, X_test, Y_test):
+#     for tree in trees:
+#         model = ensemble.RandomForestClassifier(random_state=100, n_jobs=-1, n_estimators=tree)
+#         model = model.fit(X_train, Y_train)
+#         model_pred = model.predict(X_test)
+#         model_score = jaccard_similarity_score(Y_test, model_pred)
+#         print("Random Forest score with {} trees: {:5f}".format(tree, model_score.mean()))
+#
+#
+# tune_trees(trees, X_train, Y_train, X_test, Y_test)
 
 ########################################################################################
 #                    Feature selection
 ########################################################################################
 print("\nPerforming feature selection using Sparse SVM and RF \n")
-thresholds = [0, 1e-06, 2e-06, 5e-06, 1e-05, 2e-05, 5e-05, 0.00001, 0.00005, 0.0001, 0.0005, 0.001]
+
+# TODO
+# use defealt threshold level
+# 3 methods to feature selection, svm, rf, logistic regression
+# 200 features, -> 20 -> 10
+# draw graph to visualize different method for feature selection Y: accuracy, X: num of features
+
+C_params = [1e-13, 1e-12,1e-10, 1e-09, 1e-08, 1e-07, 1e-06, 0.00001, 0.00005, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 1000000]
+C_params.reverse()
+print(C_params)
+
 
 
 # perform feature selection using sparse svm
-def svm_feature_selection(thresholds):
-    for threshold in thresholds:
-        svm_select = SelectFromModel(linear_model.SGDClassifier(random_state=100, penalty="l1", n_jobs=-1, tol=1e-3),
-                                     threshold=threshold)
-        svm_select = svm_select.fit(X_train, Y_train)
-        svm_features = svm_select.transform(X_train)
-        print("\nWith threshold {}".format(threshold))
+def svm_feature_selection(C_params):
+    for C in C_params:
+        svm_select= svm.LinearSVC(random_state=100, penalty="l1", C=C, dual=True, tol=1e-4)
+        svm_select = SelectFromModel(svm_select).fit(X_train, Y_train)
+        svm_features= svm_select.transform(X_train)
+        print("\nWith C={}".format(C))
         print("Sparse SVM reduced number of features to {}.".format(svm_features.shape[1]))
-        svm_clf_l1 = linear_model.SGDClassifier(random_state=100, penalty="l1", n_jobs=-1, alpha=0.01, tol=1e-3)
-        svm_clf_l1 = svm_clf_l1.fit(X_train, Y_train)
+        svm_clf_l1 = linear_model.SGDClassifier(random_state=100, penalty="l1", n_jobs=-1, alpha=0.01, tol=1e-4)
         svm_clf_l1_score = kfold_model_score(svm_clf_l1, svm_features, Y_train)
         print("SGD l1 CV score after FEATURE SELECTION: {:5f}".format(svm_clf_l1_score.mean()))
 
 
-svm_feature_selection(thresholds)
-
+svm_feature_selection(C_params)
 
 # perform feature selection using rf, use mean as threshold
+thresholds = [0, 1e-06, 2e-06, 5e-06, 1e-05, 2e-05, 5e-05, 0.00001, 0.00005, 0.0001, 0.0005, 0.001]
+
+
 def rf_feature_selection(thresholds):
     for threshold in thresholds:
         rf_clf = ensemble.RandomForestClassifier(random_state=100, n_jobs=-1, n_estimators=50)
@@ -112,32 +122,3 @@ def rf_feature_selection(thresholds):
 
 
 rf_feature_selection(thresholds)
-########################################################################################
-#                 Test dataset accuracy after feature selection
-########################################################################################
-# sgd_clf_l1.fit(svm_features, Y_train)
-# rf_clf.fit(rf_features, Y_train)
-# sgd_l1_pred = sgd_clf_l1.predict(X_test[svm_features[1]])
-# rf_pred = rf_clf.predict(X_test[rf_features[1]])
-
-# measure and output accuracy
-# print("\nSimilarity score of model on testing dataset after feature selection: ")
-# sgd_l1_score = jaccard_similarity_score(Y_test, sgd_l1_pred)
-# rf_score = jaccard_similarity_score(Y_test, rf_pred)
-# print("SGD similarity score: {:5f}".format(sgd_l1_score))
-# print("Random Forest similarity score: {:5f}".format(rf_score))
-#
-# print("Confusion matrix plots")
-# # SGD
-# sgd_matrix = confusion_matrix(Y_test, sgd_l1_pred)
-# np.set_printoptions(precision=2)
-# plt.figure()
-# plot_confusion_matrix(sgd_matrix, classes=class_names, title='SVM l1 after feature selection')
-# plt.show()
-#
-# # RF
-# rf_matrix = confusion_matrix(Y_test, rf_pred)
-# np.set_printoptions(precision=2)
-# plt.figure()
-# plot_confusion_matrix(rf_matrix, classes=class_names, title='Random Forest after feature selection')
-# plt.show()
